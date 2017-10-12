3acbaada6c0270f782b4d8027f64d08a5d15ec71
Christopher Huynh <chphuynh@ucsc.edu>
2017-10-12 16:14:31 -0700

    Dialogue System Setup

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+	private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+		sentences = new Queue<string>();
+	}
+	
+	public void StartDialogue(Dialogue dialogue)
+	{
+		Debug.Log("Starting Conversation with " + dialogue.name);
+
+		sentences.Clear();
+
+		foreach(string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		DisplayNextSentence();
+	}
+
+}
