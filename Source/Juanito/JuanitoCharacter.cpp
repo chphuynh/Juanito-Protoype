22a27faa349b192f97bb7c5cb166048deebb026f
CarpeSwag <james@garbagnati.com>
2017-10-15 05:09:36 -0700

    Rotated the camera 45 degrees to better fit our desired angle.

--- Source/Juanito/JuanitoCharacter.cpp
+++ Source/Juanito/JuanitoCharacter.cpp
@@ -32,7 +32,7 @@ AJuanitoCharacter::AJuanitoCharacter()
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->bAbsoluteRotation = true; // Don't want arm to rotate when character does
 	CameraBoom->TargetArmLength = 800.f;
-	CameraBoom->RelativeRotation = FRotator(-60.f, 0.f, 0.f);
+	CameraBoom->RelativeRotation = FRotator(-60.f, -45.f, 0.f);
 	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
 
 	// Create a camera...
