741b01cbf84d6c468d12b711c346e8539231f3ca
CarpeSwag <james@garbagnati.com>
2017-10-27 05:31:43 -0700

    Started from the third person template.

--- Source/Juanito/Juanito.cpp
+++ Source/Juanito/Juanito.cpp
@@ -4,6 +4,4 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Juanito, "Juanito" );
-
-DEFINE_LOG_CATEGORY(LogJuanito)
  
--- Source/Juanito/JuanitoGameMode.cpp
+++ Source/Juanito/JuanitoGameMode.cpp
@@ -1,17 +1,13 @@
 // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 
 #include "JuanitoGameMode.h"
-#include "JuanitoPlayerController.h"
 #include "JuanitoCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AJuanitoGameMode::AJuanitoGameMode()
 {
-	// use our custom PlayerController class
-	PlayerControllerClass = AJuanitoPlayerController::StaticClass();
-
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
